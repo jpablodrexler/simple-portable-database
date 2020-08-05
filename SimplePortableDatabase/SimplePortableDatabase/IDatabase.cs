206f71b7130b99eb50a92a3edb52601e3e1969fc
Juan Pablo Drexler <jpablodrexler@outlook.com>
2020-08-05 19:16:06 -0300

    Initial commit of source code.

---
+++ SimplePortableDatabase/SimplePortableDatabase/IDatabase.cs
@@ -0,0 +1,23 @@
+﻿using System.Data;
+
+namespace SimplePortableDatabase
+{
+    public interface IDatabase
+    {
+        string DataDirectory { get; }
+        char Separator { get; }
+        Diagnostics Diagnostics { get; }
+        void Initialize(string dataDirectory, char separator);
+        DataTable ReadDataTable(string tableName);
+        void WriteDataTable(DataTable dataTable);
+        string GetBlobsDirectory(string dataDirectory);
+        string GetCsvFromDataTable(DataTable table, char separator);
+        DataTable GetDataTableFromCsv(string csv, char separator, string tableName);
+        string GetTablesDirectory(string dataDirectory);
+        void InitializeDirectory(string dataDirectory);
+        object ReadBlob(string blobName);
+        string ResolveBlobFilePath(string dataDirectory, string thumbnailsFileName);
+        string ResolveTableFilePath(string dataDirectory, string entityName);
+        void WriteBlob(object blob, string blobName);
+    }
+}
