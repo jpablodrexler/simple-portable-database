032c360c8a24e91b6a205feefe72eec99ed4feef
Juan Pablo Drexler <jpablodrexler@outlook.com>
2020-08-05 22:26:05 -0300

    Adds support for code coverage reports.

---
+++ SimplePortableDatabase/test-with-coverage.bat
@@ -0,0 +1,5 @@
+dotnet test /p:CollectCoverage=true /p:CoverletOutput=TestResults/ /p:CoverletOutputFormat=lcov /p:Exclude="[*Tests*]" /p:ExcludeByAttribute="GeneratedCodeAttribute" /p:ExcludeByAttribute="ExcludeFromCodeCoverageAttribute"
+
+dotnet tool install dotnet-reportgenerator-globaltool --tool-path tools
+.\tools\reportgenerator.exe -reports:SimplePortableDatabase.Tests\TestResults\coverage.info -targetdir:.\TestResults\
+start .\TestResults\index.htm
